eabc0a15d809d2eda30c7e0a340656590accb6d2
crowl1 <47753606+crowl1@users.noreply.github.com>
2020-05-25 18:52:29 +0300

    створено базові класи в model

---
+++ M/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M
+{
+    class Manager
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public long ExecutionTime { get; set; }
+        public long ReleaseTime { get; set; }
+    }
+}
---
+++ M/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public string NameCustomer { get; set; }
+        public long TimeLeft { get; set; }
+    }
+}
